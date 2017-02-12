ef38b430aeec5836d206ecd18c3b46c3880290c1
Chris Young <chris.young@tinder.com>
2017-02-12 10:56:13 -0800

    Initial Commit

---
+++ hello-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-world
+//
+//  Created by Chris Young on 2/12/17.
+//  Copyright © 2017 Chris Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
